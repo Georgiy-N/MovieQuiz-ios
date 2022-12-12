bf25794cd7c86dd6dfe6b49c85ec14e70fb4d7c2
Georgiy-N <neguritsageorgiy@gmail.com>
2022-12-12 15:36:41 +0700

    MovieQuizHomeTask_4sprint

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,11 @@
-import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static let ypBlack = UIColor(named: "YP Black (iOS)")!
+    static let ypWhite = UIColor(named: "YP White (iOS)")!
+    static let ypGreen = UIColor(named: "YP Green (iOS)")!
+    static let ypRed = UIColor(named: "YP Red (iOS)")!
+    static let ypBackground = UIColor(named: "YP Background (iOS)")!
+    static let ypGray = UIColor(named: "YP Gray (iOS)")!
+}
+
